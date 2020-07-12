1287f962dac7a76554cc26e022dc63fdb3c54a11
Egor-kl <57603709+Egor-kl@users.noreply.github.com>
2020-07-12 21:30:52 +0300

    feat: add calendar view

---
+++ src/EasyMeeting.WebApp/Controllers/CalendarController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyMeeting.WebApp.Controllers
+{
+    /// <summary>
+    /// Calendar controller.
+    /// </summary>
+    public class CalendarController : Controller
+    {
+        public CalendarController()
+        {
+        }
+
+        /// <summary>
+        /// Return calendar VIEW.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Calendar()
+        {
+            return View("~/Views/Calendar/Calendar.cshtml");
+        }
+    }
+}
