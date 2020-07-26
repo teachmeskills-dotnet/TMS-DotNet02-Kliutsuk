01997d0906f93349cde54e2a430251e8eab6b3f9
Egor-kl <57603709+Egor-kl@users.noreply.github.com>
2020-07-26 17:49:38 +0300

    feat: add event model

---
+++ src/EasyMeeting.WebApp/Controllers/MeetingController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyMeeting.WebApp.Controllers
+{
+    /// <summary>
+    /// Meeting controller.
+    /// </summary>
+    public class MeetingController : Controller
+    {
+        public IActionResult Index()
+        {
+            if(User.Identity.IsAuthenticated)
+            {
+                return View("~/Views/Event/index.cshtml");
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+    }
+}
