c7c26bb212488932c90029125fdf6b5a12956b26
Egor-kl <57603709+Egor-kl@users.noreply.github.com>
2020-07-27 11:39:38 +0300

    fix: resolve conflict

---
+++ src/EasyMeeting.BLL/Constants/EmailConstants.cs
@@ -0,0 +1,31 @@
+﻿namespace EasyMeeting.BLL.Constants
+{
+    class EmailConstants
+    {
+        /// <summary>
+        /// smtp for google.
+        /// </summary>
+        public const string SMTP_GOOGLE = "smtp.gmail.com";
+
+        /// <summary>
+        /// smtp for google.
+        /// </summary>
+        public const string SMTP_YANDEX = "smtp.yandex.ru";
+
+        /// <summary>
+        /// Message sender address.
+        /// </summary>
+        public const string MESSAGE_SENDER_ADDRESS = "";
+
+        /// <summary>
+        /// Organization name.
+        /// </summary>
+        public const string DISPLAY_NAME = "Easy meeting";
+
+        /// <summary>
+        /// Passwor email sender.
+        /// </summary>
+        public const string EMAIL_PASSWORD = "";
+
+    }
+}
