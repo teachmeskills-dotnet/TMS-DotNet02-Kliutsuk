ace03f3d49be9f370cf11708d61bdb9fdd82dc9d
Egor-kl <57603709+Egor-kl@users.noreply.github.com>
2020-09-01 19:20:30 +0300

    feat: add new home view form

---
+++ src/EasyMeeting.WebApp/wwwroot/js/home.js
@@ -0,0 +1,8 @@
+﻿var hiddenElement = document.getElementById("about");
+var btn = document.querySelector('.ab');
+
+function handleButtonClick() {
+    hiddenElement.scrollIntoView({ block: "center", behavior: "smooth" });
+}
+
+btn.addEventListener('click', handleButtonClick);
